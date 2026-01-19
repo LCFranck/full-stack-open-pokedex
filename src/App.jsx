cc7caaaa89d5f334e2db09f8823bd57eafc9c845
LCFranck <cassandra.franck@gmail.com>
2026-01-19 14:19:54 +0200

    testing job failed

--- src/App.jsx
+++ src/App.jsx
@@ -16,9 +16,9 @@ const App = () => {
   const match = useMatch('/pokemon/:name')
   const { data: pokemonList, error, isLoading } = useApi('https://pokeapi.co/api/v2/pokemon/?limit=50', mapResults)
 
-  if (isLoading) {
-    return <LoadingSpinner />
-  }
+          if (isLoading) {
+            return <LoadingSpinner />
+          }                           
   if (error) {
     return <ErrorMessage error={error} />
   }
